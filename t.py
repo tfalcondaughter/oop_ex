83e87b90f31d8df8e5a2f46ea9caacf72705ffdc
Tetiana Sokolova <tanya.falcondaughter@gmail.com>
2021-06-15 11:01:17 +0300

    5

---
+++ t.py
@@ -0,0 +1,15 @@
+import unittest
+
+import f
+
+
+class TestF(unittest.TestCase):
+    def setUp(self):
+        self.res = f.find_b('abbbbabbab')
+
+    def test_word_abbbbabbab(self, res):
+        self.assertEqual(self.res, (7, 4))
+
+
+if __name__ == "__main__":
+    unittest.main()
