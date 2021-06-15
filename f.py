ea6722f65b0650474fc965740d7481bc7b2461a6
Tetiana Sokolova <tanya.falcondaughter@gmail.com>
2021-06-15 10:42:47 +0300

    2

--- f.py
+++ f.py
@@ -1,6 +1,15 @@
 def find_b(word):
     b = 0
     bb = 0
+    bb_c = 0
     for i in range(len(word)):
         if word[i] == 'b':
             b += 1
+            bb_c += 1
+        else:
+            bb_c = 0
+
+        if bb_c == 2:
+            bb += 1
+
+    return b, bb
